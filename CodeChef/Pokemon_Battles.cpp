cf41ae3f15d351edeb4acd75824953cd41332b5c
Vasu_Bhanderi <vbbhanderi1312@gmail.com>
2022-09-12 15:46:13 +0530

    12-09-2022

--- CodeChef/Pokemon_Battles.cpp
+++ CodeChef/Pokemon_Battles.cpp
@@ -24,76 +24,44 @@ signed main()
         int n;
         cin >> n;
         int a[n], b[n];
-        vector<int> v1;
 
         for (int i = 0; i < n; i++)
             cin >> a[i];
         for (int i = 0; i < n; i++)
             cin >> b[i];
 
-        int aw[n], bw[n];
+        int aw[n];
         for (int i = 0; i < n; i++)
             aw[i] = a[i];
-        for (int i = 0; i < n; i++)
-            bw[i] = b[i];
 
         sort(aw, aw + n);
-        sort(bw, bw + n);
 
-        map<int, int> ma, mb, maw, mbw;
-
+        map<int, int> m;
         for (int i = 0; i < n; i++)
         {
-            ma[a[i]] = i;
-            mb[b[i]] = i;
+            m[a[i]] = b[i];
         }
-        for (int i = 0; i < n; i++)
+        int ar[n];
+        int k = n - 1;
+        for (auto it : m)
         {
-            maw[aw[i]] = ma[aw[i]];
-            mbw[bw[i]] = mb[bw[i]];
+            ar[k] = it.second;
+            k--;
         }
+        int ans = 1;
 
-        int ara[n], arb[n];
-
-        int j = 0;
-        for (auto it : maw)
+        for (int i = 1; i < n; i++)
         {
-            ara[j] = it.second;
-            j++;
-        }
-        j = 0;
-        for (auto it : mbw)
-        {
-            arb[j] = it.second;
-            j++;
-        }
-
-        int s[n];
-        for (int i = 0; i < n; i++)
-        {
-            // int c = 0;
-            set<int> s1;
-            int k = 0;
-            while (ara[k] != i)
+            bool increment = true;
+            for (int j = 0; j < i;j++)
             {
-                s1.insert(ara[k]);
-                k++;
+                if (ar[j] > ar[i])
+                {
+                    increment = false;
+                    break;
+                }
             }
-            k = 0;
-            while (arb[k] != i)
-            {
-                s1.insert(arb[k]);
-                k++;
-            }
-
-            s[i] = s1.size();
-        }
-
-        int max = *max_element(s, s + n);
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            if (s[i] == max)
+            if (increment)
             {
                 ans++;
             }
